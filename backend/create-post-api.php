bcd28dcfcf3e1513a7f8fa8594d936d3cfe2661d
= <motasem.yakhni@gmail.com>
2022-09-14 20:11:28 +0300

    create post api fixes

--- backend/create-post-api.php
+++ backend/create-post-api.php
@@ -3,10 +3,9 @@
     $data = json_decode(file_get_contents("php://input"));
     $userID = $data->userID;
     $content = $data->content;
-    $postPhoto = $data->postPhoto;
 
-    $stmt = $mysqli->prepare("INSERT INTO posts(user_id, content, photo) VALUES(?, ?, ?);");
-    $stmt->bind_param("dss", $userID, $content, $postPhoto);
+    $stmt = $mysqli->prepare("INSERT INTO posts(user_id, content) VALUES(?, ?);");
+    $stmt->bind_param("ds", $userID, $content);
     if($stmt->execute()) {
         echo json_encode(["success" => true]);
     }else {
