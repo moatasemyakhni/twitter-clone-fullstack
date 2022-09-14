fd37c0a1d65574886d3bd51745a612ac48561db4
= <motasem.yakhni@gmail.com>
2022-09-14 18:22:53 +0300

    create search api

---
+++ backend/search-api.php
@@ -0,0 +1,17 @@
+<?php
+    include('connection.php');
+    $data = json_decode(file_get_contents("php://input"));
+    $nameUsername = $data->nameUsername;
+
+    $stmt = $mysqli->prepare("SELECT * FROM users WHERE name LIKE concat('%', ?, '%') OR username LIKE concat('%', ?, '%');");
+    $stmt->bind_param("ss", $nameUsername, $nameUsername);
+    if(!$stmt->execute()) {
+        die("Search Execution Failed");
+    }
+    $results = $stmt->get_result();
+    $response = [];
+    while($result = $results->fetch_assoc()) {
+        $response[] = $result;
+    }
+    
+    echo json_encode($response);
