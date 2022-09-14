576465dc61f55bc3f270f73293364c65b917bcc6
= <motasem.yakhni@gmail.com>
2022-09-14 19:20:41 +0300

    post api updates

--- backend/create-post-api.php
+++ backend/create-post-api.php
@@ -1,13 +1,12 @@
 <?php
     include('connection.php');
     $data = json_decode(file_get_contents("php://input"));
-    $content = $data->content;
-    $username = $data->username;
-    $postPhoto = $data->postPhoto;
     $userID = $data->userID;
+    $content = $data->content;
+    $postPhoto = $data->postPhoto;
 
-    $stmt = $mysqli->prepare("INSERT INTO posts(content, username, photo, user_id) VALUES(?, ?, ?, ?);");
-    $stmt->bind_param("sssd", $content, $username, $postPhoto, $userID);
+    $stmt = $mysqli->prepare("INSERT INTO posts(user_id, content, photo) VALUES(?, ?, ?);");
+    $stmt->bind_param("dss", $userID, $content, $postPhoto);
     if($stmt->execute()) {
         echo json_encode(["success" => true]);
     }else {
