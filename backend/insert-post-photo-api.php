ec2cabf676a3f5039c8b016726ab2c275ec04c68
= <motasem.yakhni@gmail.com>
2022-09-16 00:00:17 +0300

    create insert photo for post api

---
+++ backend/insert-post-photo-api.php
@@ -0,0 +1,13 @@
+<?php
+    include('connection.php');
+    $data = json_decode(file_get_contents("php://input"));
+    $postID = $data->postID;
+    $photo = $data->photo;
+
+    $stmt = $mysqli->prepare("INSERT INTO post_photos(post_id, photo) VALUES(?, ?);");
+    $stmt->bind_param("ds", $postID, $photo);
+    if($stmt->execute()) {
+        echo json_encode(["success" => true]);
+    }else {
+        echo json_encode(["success" => false]);
+    }
