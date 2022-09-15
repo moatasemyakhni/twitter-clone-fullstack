49210604756529bb170e4f7a19067445e671b361
= <motasem.yakhni@gmail.com>
2022-09-15 18:34:40 +0300

    create email getter api

---
+++ backend/get-email-api.php
@@ -0,0 +1,13 @@
+<?php
+    include('connection.php');
+    $data = json_decode(file_get_contents("php://input"));
+    $userID = $data->userID;
+
+    $stmt = $mysqli->prepare("SELECT email from email_users WHERE user_id=?");
+        $stmt->bind_param("d", $userID);
+        if(!$stmt->execute()) {
+            die("Email Execution Failed");
+        }
+        $results = $stmt->get_result();
+        $result = $results->fetch_assoc();
+        echo json_encode(["email" => $result['email']]);
