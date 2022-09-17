665b64edb2ca091d1e21bb8c86f1c152a5c2d171
= <motasem.yakhni@gmail.com>
2022-09-17 08:11:48 +0300

    add more style to the forms

--- frontend/js/signup-form.js
+++ frontend/js/signup-form.js
@@ -1,1 +1,14 @@
 
+const container = document.getElementById('signup-container')
+const name = document.getElementById('name')
+const username = document.getElementById('username')
+const pwd = document.getElementById('password')
+const pwdRepeat = document.getElementById('password-repeat')
+const dob = document.getElementById('dob')
+const signupBtn = document.getElementById('signup-btn')
+
+const callSignupFormBtn = document.getElementById('signup-page-btn')
+
+callSignupFormBtn.addEventListener('click', () => {
+    const body = document.body
+})
