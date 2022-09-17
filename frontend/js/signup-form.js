c392f2e02688e713fd4d372be7369375b875bc85
= <motasem.yakhni@gmail.com>
2022-09-17 11:38:14 +0300

    signup handlers

--- frontend/js/signup-form.js
+++ frontend/js/signup-form.js
@@ -1,4 +1,3 @@
-
 const container = document.getElementById('signup-container')
 const name = document.getElementById('name')
 const username = document.getElementById('username')
@@ -8,17 +7,97 @@ const pwd = document.getElementById('password')
 const pwdRepeat = document.getElementById('password-repeat')
 const dob = document.getElementById('dob')
 const signupBtn = document.getElementById('signup-btn')
-
 const callSignupFormBtn = document.getElementById('signup-page-btn')
+const inputs = document.querySelectorAll('.input-box')
+const errSection = document.querySelector('.error-section')
+const errTxt = document.getElementById('error-txt')
 
-const closeBtn = document.getElementById('x-close')
-closeBtn.addEventListener('click', () => {
-    container.classList.add('view-none')
+//validators
+
+const setErrorMessage = (msg) => {
+    errSection.classList.remove('view-none')
+    errTxt.textContent = msg
+}
+
+const validators = () => {
+    inputs.forEach((input) => {
+        input.addEventListener('input', () => {
+            //empty fields handler
+            if(!input.value) {
+                setErrorMessage(`${input.id} is required`)
+                return
+            }else {
+                errSection.classList.add('view-none')
+            }
+            // name handler
+            if(input.id === name.id) {
+                const limit = 50
+                const exp = /^\w{0,50}$/
+                const nameVal = input.value
+                if(!nameVal.match(exp)) {
+                    setErrorMessage(`${input.id} should contain english characters, numbers and less than ${limit} chars`)
+                    return
+                }
+                errSection.classList.add('view-none')
+                console.log("name is true")
+            }
+            //username handler
+            if(input.id === username.id) {
+                const limit = 50
+                const exp = /^@\w{0,50}$/
+                const usernameVal = input.value
+                if(!usernameVal.match(exp)) {
+                    setErrorMessage(`${input.id} should start with '@', then english characters, numbers and less than ${limit} chars`)
+                    return
+                }
+                errSection.classList.add('view-none')
+                console.log("username is true")
+            }
+            //Lebanese phone handler
+            if(input.id === phone.id) {
+                const exp1 = /^[+]9613\d{6}$/
+                const exp2 = /^[+]961(71|76|70|78)\d{6}$/
+                const phoneVal = input.value
+                if(!phoneVal.match(exp1) && !phoneVal.match(exp2)) {
+                    setErrorMessage(`${input.value} is not a Lebanese number`)
+                    return
+                }
+                errSection.classList.add('view-none')
+                console.log('phone is true')
+            }
+            //email handler
+            if(input.id === email.id) {
+                const exp = /^(\w([\.-]?\w)*)+@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+                const emailVal = input.value
+                if(!emailVal.match(exp)) {
+                    setErrorMessage(`Try format example@gmail.com. ${input.value} is not a valid email`)
+                    return
+                }
+                errSection.classList.add('view-none')
+                console.log('email is true')
+            }
+        })
+    })
+
+    if(!name.value || !username.value || (!phone.value && !email.value) || !pwd.value || !pwdRepeat.value || !dob.value) {
+        setErrorMessage(`All fields are required`)
+        return
+    }
+    if(pwd.value !== pwdRepeat.value) {
+        setErrorMessage(`Passwords do not match`)
+        return
+    }
+}
+
+validators()
+
+signupBtn.addEventListener('click', (e) => {
+
+    e.preventDefault()
 })
 
-callSignupFormBtn.addEventListener('click', () => {
-    container.classList.remove('view-none')
-})
+
+
 
 const toPhoneLink = document.getElementById('to-phone')
 const toEmailLink = document.getElementById('to-email')
@@ -34,4 +113,13 @@ toEmailLink.addEventListener('click', () => {
     emailOrPhoneContainer.forEach((cont) => {
         cont.classList.toggle('view-none')
     })
+})
+
+const closeBtn = document.getElementById('x-close')
+closeBtn.addEventListener('click', () => {
+    container.classList.add('view-none')
+})
+
+callSignupFormBtn.addEventListener('click', () => {
+    container.classList.remove('view-none')
 })
