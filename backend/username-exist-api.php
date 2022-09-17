0a5619f5b7b2ae39377f46ceaf720edd930a6f75
= <motasem.yakhni@gmail.com>
2022-09-17 15:57:27 +0300

    create api that checks if username exists

---
+++ backend/username-exist-api.php
@@ -0,0 +1,24 @@
+<?php
+    include('connection.php');
+    $data = json_decode(file_get_contents("php://input"));
+    $username = $data->username;
+
+    $stmt = $mysqli->prepare("SELECT username from users WHERE username=?");
+        $stmt->bind_param("s", $username);
+        if(!$stmt->execute()) {
+            die("username Execution Failed");
+        }
+        $response = [];
+        $results = $stmt->get_result();
+        while($result = $results->fetch_assoc()) {
+            $response[] = $result;
+        }
+        // $results = $stmt->get_result();
+        // $result = $results->fetch_assoc();
+        // $usernameRes = $result['username'];
+        if(count($response) <= 0) {
+            echo json_encode(['usernameExist' => false]);
+        }else {
+            echo json_encode(["usernameExist" => true]);
+        }
+        
