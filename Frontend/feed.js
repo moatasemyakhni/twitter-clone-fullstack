77fd5b6ae4fb4e05b7adbc3ee264029618d04f77
Ramzi Ashkar <ramziashkar.dev@gmail.com>
2022-09-17 14:16:47 +0300

    JS: Added logout container interactions

--- Frontend/feed.js
+++ Frontend/feed.js
@@ -1,22 +1,28 @@
 const inputimg = document.getElementById('capture');
 const cancelbtn = document.getElementById('cancel');
 const chosenimage = document.getElementById('chosenimage');
+const userprofile = document.querySelector('.user-profile');
+const logoutContainer = document.querySelector('.logout');
 
 
 // Function to display users' chosen image
 const displayChosenImg = () => {
-  chosenimage.classList.remove('hidden-img');
+  chosenimage.classList.remove('alwayshidden');
   chosenimage.src = URL.createObjectURL(event.target.files[0]);
-  cancelbtn.classList.remove('hidden-btn');
+  cancelbtn.classList.remove('alwayshidden');
 };
 
 // Function to remove users' chosen image
 const removeChosenImg = () => {
-  chosenimage.classList.add('hidden-img');
+  chosenimage.classList.add('alwayshidden');
   chosenimage.src = '';
-  cancelbtn.classList.add('hidden-btn');
+  cancelbtn.classList.add('alwayshidden');
 };
 
+// Function to display logout Container
+const logoutContainerShow = () => {
+  logoutContainer.classList.toggle("alwayshidden");
+};
 
 // Event Listeners
 inputimg.addEventListener("change", () => {
@@ -25,4 +31,8 @@ displayChosenImg();
 
 cancelbtn.addEventListener("click", () => {
 removeChosenImg();
+});
+
+userprofile.addEventListener("click", () => {
+logoutContainerShow();
 });
