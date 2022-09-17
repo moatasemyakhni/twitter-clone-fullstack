6bbada357f463a6fb114debbc5f2924e87773a25
= <motasem.yakhni@gmail.com>
2022-09-17 09:03:57 +0300

    call and close signup form

--- frontend/js/signup-form.js
+++ frontend/js/signup-form.js
@@ -2,6 +2,8 @@
 const container = document.getElementById('signup-container')
 const name = document.getElementById('name')
 const username = document.getElementById('username')
+const phone = document.getElementById('phone')
+const email = document.getElementById('email')
 const pwd = document.getElementById('password')
 const pwdRepeat = document.getElementById('password-repeat')
 const dob = document.getElementById('dob')
@@ -9,7 +11,28 @@ const signupBtn = document.getElementById('signup-btn')
 
 const callSignupFormBtn = document.getElementById('signup-page-btn')
 
+const closeBtn = document.getElementById('x-close')
+closeBtn.addEventListener('click', () => {
+    container.classList.add('view-none')
+})
+
 callSignupFormBtn.addEventListener('click', () => {
-    const body = document.body
-    
+    container.classList.remove('view-none')
+})
+
+const toPhoneLink = document.getElementById('to-phone')
+const toEmailLink = document.getElementById('to-email')
+const emailOrPhoneContainer = document.querySelectorAll('email-phone-container')
+
+// toPhoneLink.addEventListener('click', () => {
+//     emailOrPhoneContainer.forEach((container) => {
+//         console.log(container)
+//         container.classList.toggle('view-none')
+//     })
+// })
+
+toEmailLink.addEventListener('click', () => {
+    emailOrPhoneContainer.forEach((container) => {
+        container.classList.toggle('view-none')
+    })
 })
