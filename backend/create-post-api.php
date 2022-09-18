01e9d8c8eb092ea735f9d29f54d76171f944f23c
= <motasem.yakhni@gmail.com>
2022-09-18 10:13:15 +0300

    update create post api

--- backend/create-post-api.php
+++ backend/create-post-api.php
@@ -6,8 +6,17 @@
 
     $stmt = $mysqli->prepare("INSERT INTO posts(user_id, content) VALUES(?, ?);");
     $stmt->bind_param("ds", $userID, $content);
-    if($stmt->execute()) {
-        echo json_encode(["success" => true]);
-    }else {
-        echo json_encode(["success" => false]);
+    if(!$stmt->execute()) {
+        // echo json_encode(["success" => true]);
+        die("Error in create post");
     }
+    //grab the current post id
+    $query = $mysqli->prepare("SELECT max(post_id) as recent_post FROM posts");
+    if(!$query->execute()) {
+        die("Error in create post id selection");
+    }
+    $results = $query->get_result();
+    $result = $results->fetch_assoc();
+    $postID = $result['recent_post'];
+
+    echo json_encode(["success" => true, "postID" => $postID]);
