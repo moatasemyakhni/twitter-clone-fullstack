5ee64c174954c273def62a03fe7774e6c5d08c9e
= <motasem.yakhni@gmail.com>
2022-09-18 11:20:25 +0300

    update the relative path in insert post api

--- backend/insert-photo-in-post.php
+++ backend/insert-photo-in-post.php
@@ -7,17 +7,17 @@
     //$photo is in base 64
     $data = explode(',', $photo);// to get the ext
     $ext = explode(';', explode('/', $data[0])[1])[0];
-    $user = $username . "_" . time(); //unique it
+    $post = "post" . $postID . "_" . time(); //unique it
     $path = "D:\\php\\htdocs\\9-sefactory\\twitter-clone-fullstack\\frontend\\assets\\images\\usersPosts\\";
-    $completeUrl = $path . $user . "." . $ext;
+    $completeUrl = $path . $post . "." . $ext;
 
     //Actually saving the photo in the previous path
     file_put_contents($completeUrl, file_get_contents($photo));
 
     $stmt = $mysqli->prepare("INSERT INTO post_photos VALUES(?, ?);");
     $stmt->bind_param("ds", $postID, $completeUrl);
-    if($stmt->execute()) {
-        echo json_encode(["success" => true]);
+    if(!$stmt->execute()) {
+        echo json_encode(["success" => false]);
     }else {
         echo json_encode(["success" => true, "url" => $completeUrl]);
     }
